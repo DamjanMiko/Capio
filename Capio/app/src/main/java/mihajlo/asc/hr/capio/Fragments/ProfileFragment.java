805ec30522491fe6352f710f2ebd5960062866f0
marko.raguz <markoraguz@gmail.com>
2017-03-22 14:07:42 +0100

    1st try

--- Capio/app/src/main/java/mihajlo/asc/hr/capio/Fragments/ProfileFragment.java
+++ Capio/app/src/main/java/mihajlo/asc/hr/capio/Fragments/ProfileFragment.java
@@ -3,9 +3,13 @@ package mihajlo.asc.hr.capio.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import mihajlo.asc.hr.capio.R;
 
@@ -14,17 +18,77 @@ import mihajlo.asc.hr.capio.R;
  */
 public class ProfileFragment extends Fragment {
 
-
+    private View view;
+    EditText inputName;
+    EditText inputEmail;
+    EditText inputPh;
+    EditText inputLoc;
+    TextView tv;
     public ProfileFragment() {
         // Required empty public constructor
     }
 
+    private class UserInfo{
+        public String username;
+        public String phone;
+        public String address;
+        public String email;
+
+        public UserInfo(){
+
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        Log.d("MyApp","I am here");
+        view =  inflater.inflate(R.layout.fragment_profile, container, false);
+         inputName  = (EditText) view.findViewById(R.id.editText);
+         inputEmail  = (EditText) view.findViewById(R.id.editText7);
+         inputPh  = (EditText) view.findViewById(R.id.editText6);
+         inputLoc  = (EditText) view.findViewById(R.id.editText8);
+        tv = (TextView) view.findViewById(R.id.textView);
+        UserInfo theUser = new UserInfo();
+        theUser.username = "Ivica Kicmanovic";
+        theUser.phone = "+385 91 012 3456";
+        theUser.address = "Unska 3, Zagreb";
+        theUser.email = "ivica.kicmanovic@efzg.com";
+
+        //
+
+
+        inputName.setText(theUser.username,TextView.BufferType.EDITABLE);
+
+
+        inputEmail.setText(theUser.email);
+
+
+        inputPh.setText(theUser.phone);
+
+
+        inputLoc.setText(theUser.address);
+        Button clickButton = (Button) view.findViewById(R.id.button2);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                tv.setText("please");
+                System.out.println("Trying");
+
+            }
+        });
+
+        return view;
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+
+
+
+    }
 }
